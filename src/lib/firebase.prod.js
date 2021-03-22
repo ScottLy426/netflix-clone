2a7ac87c2ede927a2839880bf1c1c24468f3ae71
Scottly426 <lyscott123@gmail.com>
2021-03-22 13:29:46 -0700

    adding finished project

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,13 +6,13 @@ import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: "AIzaSyAMpsvJB-E99zuSdHKNduVsUu29rbzbfMw",
+  authDomain: "netflix-clone-fdca1.firebaseapp.com",
+  projectId: "netflix-clone-fdca1",
+  storageBucket: "netflix-clone-fdca1.appspot.com",
+  messagingSenderId: "888449951702",
+  appId: "1:888449951702:web:e927417dd95db83edce677",
+  measurementId: "G-84PRSDE6VT"
 };
 
 const firebase = Firebase.initializeApp(config);
